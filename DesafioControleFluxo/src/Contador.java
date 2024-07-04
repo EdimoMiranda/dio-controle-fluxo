57618e958ef1d5262d8064dd68014f9a8685be7c
Edimo Miranda <emc.edimo@gmail.com>
2024-07-03 23:21:30 -0300

    Controle de Fluxo - Desafio

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+// a classe ParametrosInvalidosException representa a exceção de negócio no sistema.
+class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException() {
+        super("O segundo parâmetro deve ser maior que o primeiro.");
+    }
+}
+
+public class Contador {
+    public static void main(String[] args) {
+        try (Scanner terminalScanner = new Scanner(System.in)) {
+            System.out.println("Digite o primeiro parâmetro:");
+            int parametroUm = terminalScanner.nextInt();
+            System.out.println("Digite o segundo parâmetro:");
+            int parametroDois = terminalScanner.nextInt();
+
+            try {
+                // Chamando o método contendo a lógica de contagem
+                contar(parametroUm, parametroDois);
+            } catch (ParametrosInvalidosException e) {
+                // Imprimir a mensagem da exceção
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        // Validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if (parametroUm >= parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = parametroDois - parametroUm;
+        // Realizar o loop para imprimir os números com base na variável contagem
+        for (int i = 0; i <= contagem; i++) {
+            System.out.println(contagem + i);
+        }
+    }
+}
